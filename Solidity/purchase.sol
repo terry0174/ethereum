94b309228cd51a918276ce7411e59a03c9760ae4
terry0174 <terry0174@gmail.com>
2019-01-11 11:57:34 +0800

    Create purchase.sol

---
+++ Solidity/purchase.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.22;
+
+contract Purchase {
+    
+    uint public value; //商品售價
+    address public seller; //賣家
+    address public buyer; //買家
+    
+    /**
+     * Created - 合約已生成(可由買家購買或賣家中止合約)
+     * Locked - 合約鎖定(買家已購買)
+     * Inactive - 合約中止(交易結束或賣家中止合約)
+     */
+    enum State { Created, Locked, Inactive }
+    State public state;
+
+    modifier condition(bool _condition) {
+        require(_condition);
+        _;
+    }
+
+    modifier onlyBuyer() {
+        require(msg.sender == buyer, "只有買家可以呼叫");
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "只有賣家可以呼叫");
+        _;
+    }
+
+    modifier inState(State _state) {
+        require(state == _state, "當前狀態無法呼叫此函式");
+        _;
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+
+    //建立合約,賣家存放2倍商品售價作為押金
+    //以太幣由合約帳戶鎖定。
+    constructor() public payable {
+        
+        seller = msg.sender;
+        value = msg.value / 2;
+        
+        require((2 * value) == msg.value, "Value必須為偶數");
+    }
+
+    //賣家中止合約並回收押金。
+    //只能在合約鎖定前,並由賣家調用。
+    function abort() public onlySeller inState(State.Created) {
+        
+        state = State.Inactive;
+        seller.transfer(address(this).balance);
+        
+        emit Aborted();
+    }
+
+    //買家購買商品,並支付2倍售價以太幣(含押金)。
+    //以太幣由合約帳戶鎖定。
+    function confirmPurchase() public inState(State.Created) condition(msg.value == (2 * value)) payable {
+        
+        buyer = msg.sender;
+        state = State.Locked;
+        
+        emit PurchaseConfirmed();
+    }
+
+    //買家通知合約已收到商品。
+    //釋放合約鎖定的以太幣。
+    function confirmReceived() public onlyBuyer inState(State.Locked) {
+        
+        state = State.Inactive;
+        
+        buyer.transfer(value);
+        seller.transfer(address(this).balance);
+        
+        emit ItemReceived();
+    }
+}
