2aa8d52f3578f8799ecd14d2e1eb1ef8d6d52feb
terry0174 <terry0174@gmail.com>
2019-01-19 11:04:00 +0800

    Update app.js

--- Dapp/example02/app.js
+++ Dapp/example02/app.js
@@ -1,7 +1,6 @@
 /* init web3 */
 
 web3 = new Web3(new Web3.providers.WebsocketProvider("ws://192.168.99.1:8546"));
-// web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.99.1:8545"));
 
 /* init smart contract */
 var contract = new web3.eth.Contract(abi, contractAddress);
