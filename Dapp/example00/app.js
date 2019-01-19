92b09f0d13ba5db4aefa0a323948b9a9d01b7279
terry0174 <terry0174@gmail.com>
2019-01-19 17:31:57 +0800

    Create example00

---
+++ Dapp/example00/app.js
@@ -0,0 +1,17 @@
+/* init web3 */
+if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+} else {
+    web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.99.1:8545"));
+}
+
+/* init vue */
+var app = new Vue({
+    el: '#app',
+    data: {
+    },
+    methods: {
+    },
+    mounted: function () {     
+    }
+})
