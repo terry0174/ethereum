685c7f65fe5f4a5d165f8fac3d5065b0996e54b5
terry0174 <terry0174@gmail.com>
2019-01-22 17:36:06 +0800

    Create helloWorld

---
+++ Solidity/helloWorld/helloWorld.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract helloWorld {
+
+    string public hello = "hello world";
+
+    function sayHello() public view returns (string memory) {
+        return hello;
+    }
+} 
