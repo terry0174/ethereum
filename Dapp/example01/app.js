b2955da2237b2fd1dd74e33b2b9c9bf01e24b368
terry0174 <terry0174@gmail.com>
2019-02-11 14:34:29 +0800

    Change IP

--- Dapp/example01/app.js
+++ Dapp/example01/app.js
@@ -2,7 +2,7 @@
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
 } else {
-    web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.99.100:8545"));
+    web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.99.1:8545"));
 }
 
 /* init vue */
